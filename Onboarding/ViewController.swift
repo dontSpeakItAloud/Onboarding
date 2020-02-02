e56d8e6b1e3cf2cdf609a8ca4053e24a699f682d
Rodion Kuskov <rodion@dsi.io>
2020-02-02 18:37:32 +0200

    Initial Commit

---
+++ Onboarding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Onboarding
+//
+//  Created by Rodion Kuskov on 2/2/20.
+//  Copyright © 2020 Rodion Kuskov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
